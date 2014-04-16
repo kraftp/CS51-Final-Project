b978606b5e5c06bf525bcad9977fe870049462a2
Peter <pkraft@college.harvard.edu>
2014-04-15 20:54:01 -0400

    finished sig

--- Nfa.ml
+++ Nfa.ml
@@ -2,7 +2,32 @@
 (*** PETER KRAFT LILLY SHEN KIMBERLEY YU KAT ZHOU***)
  
 open Core.Std
+open Parser
  
-(*BUILD AND NFA HERE*)
+(*BUILD AN NFA HERE*)
  
- 
+  module type AUTOMATON =
+sig
+    exception NotRecognized
+    exception TODO
+    
+    type nfa
+    
+    val tonfa : Parse.pt -> nfa
+    
+    
+end
+
+
+module Auto : AUTOMATON =
+struct
+
+    exception NotRecognized
+    exception TODO
+        
+    type nfa = string (**TODO**)
+    
+        
+    let tonfa (parse : Parse.pt) : nfa = "bob the builder"    
+
+end
