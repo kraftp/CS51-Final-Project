b17cbd42571a1205984880aa86066ec9b6a11293
Kimberley Yu <kimberleyyu@college.harvard.edu>
2014-04-18 14:54:16 -0400

    nfa

--- Nfa.ml
+++ Nfa.ml
@@ -13,6 +13,7 @@ sig
     
     type nfa
     
+    val lptr : nfa -> nfa list (* list of all pointers to empty *)
     val tonfa : Parse.pt -> nfa
     
     
@@ -25,7 +26,12 @@ struct
     exception NotRecognized
     exception TODO
         
-    type nfa = string (**TODO**)
+    type nfa = 
+	Empty
+      | Single of char * nfa ref (* char and forward pointer to next node *)
+      | Or of nfa ref * nfa ref (* forward pointers to 2 or options *)
+      | Star of nfa ref * nfa ref (* forward pointers into and out of closure *)
+    
     
         
     let tonfa (parse : Parse.pt) : nfa = "bob the builder"    
