3ff499aad6e11627d6b6bc290b1b93c999213acc
Peter <pkraft@college.harvard.edu>
2014-04-18 15:22:10 -0400

    lptr written

--- Nfa.ml
+++ Nfa.ml
@@ -13,7 +13,6 @@ sig
     
     type nfa
     
-    val lptr : nfa -> nfa list (* list of all pointers to empty *)
     val tonfa : Parse.pt -> nfa
     
     
@@ -27,13 +26,20 @@ struct
     exception TODO
         
     type nfa = 
-	Empty
+	    Empty
       | Single of char * nfa ref (* char and forward pointer to next node *)
       | Or of nfa ref * nfa ref (* forward pointers to 2 or options *)
       | Star of nfa ref * nfa ref (* forward pointers into and out of closure *)
+      
+    let rec lptr (input : nfa) : nfa ref list =
+        match input with
+          Empty -> failwith "should have been caught earlier"
+        | Single(_, ptr) -> if !ptr = Empty then [ptr] else lptr !ptr 
+        | Or(ptr1, ptr2) -> (lptr !ptr1)@(lptr !ptr2)
+        | Star (_, ptr2) -> if !ptr2 = Empty then [ptr2] else lptr !ptr2 
     
     
         
-    let tonfa (parse : Parse.pt) : nfa = "bob the builder"    
+    let tonfa (parse : Parse.pt) : nfa = Empty    
 
 end
