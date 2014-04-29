d3c6af7592acbe324147729a6479d380045dced0
Kat Zhou <kathleenzhou@college.harvard.edu>
2014-04-28 21:39:28 -0400

    command line pkraftpkraftpkraftpkraft

--- Regex.ml
+++ Regex.ml
@@ -9,9 +9,32 @@ open Parser
  
 (*IMPLEMENT I/O HERE*)
  
+ (* WTF PETER 
  let cs = Parse.parse "(a*a|b|as|da)|q*sdfs(f|s)" in
  (* Auto.makedot cs *)
      let cs2 = Auto.to_nfa cs in 
-  assert((Emulate.eval "b" cs2)&&(Emulate.eval "qqqqsdfsf" cs2)&&(not (Emulate.eval "sa" cs2)))
+  assert((Emulate.eval "b" cs2)&&(Emulate.eval "qqqqsdfsf" cs2)&&(not (Emulate.eval "sa" cs2))) *)
+  
+  let command = 
+    Command.basic
+    ~summary: "Matches strings based on inputted regular expression!"
+    Command.Spec.(
+      empty
+      +> flag "-d" no_arg ~doc:"Generate DOT code"
+      +> anon ("regex" %: string)
+      +> anon ("input" %: string)
+    )
+    (fun if_dot regex input () -> 
+      let parse = Parse.parse regex in
+      if if_dot then 
+        ignore(Auto.makedot parse)
+      else
+        let nfa = Auto.to_nfa parse in
+        if (Emulate.eval input nfa) then Printf.printf ("Matched! \n")
+        else Printf.printf ("No match ): sry bbz \n")
+    )
+    
+    let () = Command.run command
+    
 
  
